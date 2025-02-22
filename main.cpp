5c864b539b18b539f1673d208dd3977af35c23cb
Levi-Klam <klamerlj@gmail.com>
2025-02-22 15:00:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <string>
 #include "game.h"
@@ -12,18 +11,4 @@ int main() {
 
     return 0;
 }
-=======
-#include <iostream>
-#include <string>
-#include "game.h"
 
-
-int main() {
-    Game game; // Create a game object
-    game.startGame(); // Call the function to start the game
-
-    //std::cout << curMessage << std::endl;
-
-    return 0;
-}
->>>>>>> 1f1e89e0885b47046d1924395484d408c8f6fa77
